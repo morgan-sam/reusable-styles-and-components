b1b22c3bb3024683981d429067067c50a451b7b5
Sam <samuel_morgan_@outlook.com>
2020-09-21 22:08:57 +0100

    page & border class

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function App() {
-  return <div className="center">App</div>;
+  return (
+    <div className="page center">
+      <div className="p2 border">App</div>
+    </div>
+  );
 }
 
 export default App;
