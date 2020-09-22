ed6483791cd812badd52c9a5fbb99cec5d65b629
Sam <samuel_morgan_@outlook.com>
2020-09-22 15:40:15 +0100

    horizontal class

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ function App() {
     <div className="page center p2">
       <div className="m1 p2 border">App</div>
       <div className="m1 p2 border">Now</div>
+      <div className="horizontal">
+        <div className="m1 p2 border">App</div>
+        <div className="m1 p2 border">App</div>
+        <div className="m1 p2 border">App</div>
+      </div>
     </div>
   );
 }
