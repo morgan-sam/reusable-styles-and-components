d83570af5892ceb6387bc22a55fb77406364f846
Sam <samuel_morgan_@outlook.com>
2020-09-22 15:38:23 +0100

    Page now flows downward

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from "react";
 
 function App() {
   return (
-    <div className="page center">
-      <div className="p2 border">App</div>
+    <div className="page center p2">
+      <div className="m1 p2 border">App</div>
+      <div className="m1 p2 border">Now</div>
     </div>
   );
 }
