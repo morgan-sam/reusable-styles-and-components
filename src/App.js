da5592e330427a79727e5026a65ba898c084c95c
Sam <samuel_morgan_@outlook.com>
2020-09-22 22:41:37 +0100

    bottom/top classes

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import React from "react";
 function App() {
   return (
     <div className="page center p2">
-      <div className="m1 p2 border">App</div>
-      <div className="m1 p2 border">App</div>
-      <div className="container center horizontal">
+      <div className="container top horizontal">
         <div className="container vertical">
           <div className="m1 p2 border">App</div>
           <div className="m1 p2 border">App</div>
@@ -13,13 +11,15 @@ function App() {
         </div>
         <div className="m1 p2 border">App</div>
         <div className="m1 p2 border">App</div>
-        <div className="m1 p2 border">App</div>
-        <div className="m1 p2 border">App</div>
+      </div>
+      <div className="container bottom horizontal">
         <div className="container vertical">
           <div className="m1 p2 border">App</div>
           <div className="m1 p2 border">App</div>
           <div className="m1 p2 border">App</div>
         </div>
+        <div className="m1 p2 border">App</div>
+        <div className="m1 p2 border">App</div>
       </div>
     </div>
   );
