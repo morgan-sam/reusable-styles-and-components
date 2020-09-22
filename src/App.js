1c60c9bd1e29be15c6b7f8993375ef33ca5e705e
Sam <samuel_morgan_@outlook.com>
2020-09-22 15:45:33 +0100

    Added container class

--- src/App.js
+++ src/App.js
@@ -4,11 +4,22 @@ function App() {
   return (
     <div className="page center p2">
       <div className="m1 p2 border">App</div>
-      <div className="m1 p2 border">Now</div>
-      <div className="horizontal">
+      <div className="m1 p2 border">App</div>
+      <div className="container center horizontal">
+        <div className="container vertical">
+          <div className="m1 p2 border">App</div>
+          <div className="m1 p2 border">App</div>
+          <div className="m1 p2 border">App</div>
+        </div>
         <div className="m1 p2 border">App</div>
         <div className="m1 p2 border">App</div>
         <div className="m1 p2 border">App</div>
+        <div className="m1 p2 border">App</div>
+        <div className="container vertical">
+          <div className="m1 p2 border">App</div>
+          <div className="m1 p2 border">App</div>
+          <div className="m1 p2 border">App</div>
+        </div>
       </div>
     </div>
   );
