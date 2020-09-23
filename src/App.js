7753c0a0001e3a8480a65b54e46daa6dda4dc77d
Sam <samuel_morgan_@outlook.com>
2020-09-23 23:18:16 +0100

    Added header class

--- src/App.js
+++ src/App.js
@@ -2,24 +2,33 @@ import React from "react";
 
 function App() {
   return (
-    <div className="page center p2">
-      <div className="container top horizontal">
-        <div className="container vertical">
-          <div className="m1 p2 border">App</div>
-          <div className="m1 p2 border">App</div>
-          <div className="m1 p2 border">App</div>
+    <div className="page">
+      <div className="header border">
+        <div className="container horizontal">
+          <div className="m1 p1 border">1</div>
+          <div className="m1 p1 border">2</div>
+          <div className="m1 p1 border">3</div>
         </div>
-        <div className="m1 p2 border">App</div>
-        <div className="m1 p2 border">App</div>
       </div>
-      <div className="container bottom horizontal">
-        <div className="container vertical">
-          <div className="m1 p2 border">App</div>
+      <div className="body center p2">
+        <div className="container top horizontal">
+          <div className="container vertical">
+            <div className="m1 p2 border">App</div>
+            <div className="m1 p2 border">App</div>
+            <div className="m1 p2 border">App</div>
+          </div>
           <div className="m1 p2 border">App</div>
           <div className="m1 p2 border">App</div>
         </div>
-        <div className="m1 p2 border">App</div>
-        <div className="m1 p2 border">App</div>
+        <div className="container bottom horizontal">
+          <div className="container vertical">
+            <div className="m1 p2 border">App</div>
+            <div className="m1 p2 border">App</div>
+            <div className="m1 p2 border">App</div>
+          </div>
+          <div className="m1 p2 border">App</div>
+          <div className="m1 p2 border">App</div>
+        </div>
       </div>
     </div>
   );
